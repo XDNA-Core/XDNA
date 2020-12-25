7cdac9166476c786bd3a08e5ec5acf4bcafc8f9c
XDNA-Core <hllmr1@gmail.com>
2020-12-25 07:50:36 +0300

    v2.1.2.0 Seed-nodes and dns-seeder changes for better connection and syncing

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -276,7 +276,7 @@ public:
         assert(hashGenesisBlock == uint256("000003b39d72ad4da1eb2ef2d044032dd95750cc25c435ecad2a236dd22b99fe"));
         assert(genesis.hashMerkleRoot == uint256("89370975b13f97d8f9cfc373b0e9d5cc0e2e06b8dc283c76824e4df03ca2d60a"));
 
-        vSeeds.push_back(CDNSSeedData("xdna.io", "seeds.seeder01.xdna.io"));     // Primary DNS Seeder
+        vSeeds.push_back(CDNSSeedData("xdna.online", "seeds.seeder01.xdna.online"));     // Primary DNS Seeder
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 75);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 8);
